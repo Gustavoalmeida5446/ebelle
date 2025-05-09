5a1ee89e2edce4e2965c0bea4205931f58b64137
Gustavo <gtva5446@gmail.com>
2025-05-09 19:59:17 -0300

    feat: footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,27 @@
 import logoFooter from "../assets/logoFooter.png";
+import { useNavigate } from "react-router";
 
 function Footer() {
+  const navigate = useNavigate();
   return (
     <footer className="footer">
-      <img src={logoFooter} alt="Logo e-belle" className="footer-logo" />
-      <p>&copy; 2025 Vilarika. Todos os direitos reservados.</p>
+      <img
+        src={logoFooter}
+        alt="Logo e-belle"
+        className="footer-logo"
+        onClick={() => navigate("/ebelle")}
+      />
+      <p>
+        &copy; 2025{" "}
+        <a
+          style={{ color: "var(--white)" }}
+          href="http://vilarika.com.br/"
+          target="_blank"
+        >
+          Vilarika
+        </a>
+        . Todos os direitos reservados.
+      </p>
     </footer>
   );
 }
